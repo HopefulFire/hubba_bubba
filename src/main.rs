a81efa2fbcc18445738f13b1937e4e14f39effbe
Jade Lynn Masker <jadelynn@masker.family>
2020-12-23 18:35:13 -0500

    some fun with rust_decimal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use rust_decimal::prelude::*;
+
+fn main()
+{
+    let n1 = Decimal::new(25_0_000_000_000, 10);
+    let n2 = Decimal::new(10_0_000_000_000, 10);
+    let n3 = n1 + n2;
+    println!("{}", n3);
 }
